e3668f92a2b2c8caac7f3aa56af21f8a206fabe8
Mandraszewsky <mandrah@o2.pl>
2025-01-07 15:00:25 +0100

    sharing state across components & some minor improvments

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,27 +1,31 @@
 import { useImperativeHandle, useRef } from "react";
 
-export default function ResultModal({ ref, result, targetTime }) {
-
+export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
   const dialog = useRef();
+  const userLost = remainingTime <= 0;
+  const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
   useImperativeHandle(ref, () => {
     return {
       open() {
-        dialog.current.showMOdal();
-      }
+        dialog.current.showModal();
+      },
     };
   });
 
   return (
-    <dialog className="result-modal" ref={dialog}>
-      <h2>You {result} </h2>
+    <dialog className="result-modal" ref={dialog} onClose={onReset}>
+      {userLost && <h2>You lost </h2>}
+      {!userLost && <h2>Your score: {score} </h2>}
       <p>
         The target time was <strong>{targetTime}</strong> seconds.
       </p>
       <p>
-        You stopped the timer with <strong>X seconds left.</strong>
+        You stopped the timer with{" "}
+        <strong>{formattedRemainingTime} seconds left.</strong>
       </p>
-      <form method="dialog">
+      <form method="dialog" onSubmit={onReset}>
         <button> Close </button>
       </form>
     </dialog>
